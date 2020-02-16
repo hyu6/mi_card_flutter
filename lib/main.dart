fb0c2a112f78c4ba5f815643d081602d39e75147
Haoran Yu <haoranyu6@gmail.com>
2020-02-16 15:19:54 -0800

    Update the layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+            padding: EdgeInsets.all(10),
+            child: Text("Hello"),
+          ),
+        ),
+      ),
+    );
+  }
 }
